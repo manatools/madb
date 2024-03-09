b1302c4b6931a7cffea9a9f473c00be59ec5d00b
Papoteur <papoteur@mageia.org>
2024-03-09 12:09:20 +0100

    config: use standard codes for architectures

--- madb/config.py
+++ madb/config.py
@@ -4,9 +4,9 @@ DATA_PATH = "/home/yves/dev/madb2"
 DEF_GROUPS_FILE = "/usr/share/rpmlint/config.d/distribution.exceptions.conf"
 ARCHES = {
     "x86_64": "x86 64bits",
-    "x86_32": "x86 32bits",
-    "arm_64": "Arm 64bits",
-    "arm_32": "Arm 32bits v7hl",
+    "i586": "x86 32bits",
+    "aarch64": "Arm 64bits",
+    "armv7hl": "Arm 32bits v7hl",
 }
 DISTRIBUTION = {
     "cauldron": "Mageia cauldron",
