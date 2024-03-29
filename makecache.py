e5506a27cc0eb673ffb4949e9942fd17f26c7c4c
Papoteur <papoteur@mageia.org>
2024-03-29 07:21:40 +0100

    Add indication of elapsed time in makecache

--- makecache.py
+++ makecache.py
@@ -3,8 +3,13 @@
 import madb.config as config
 from madb.dnf5madbbase import Dnf5MadbBase
 import logging
+import datetime
+import humanize
+
+start = datetime.datetime.now()
 
 for distro in iter(config.DISTRIBUTION.keys()):
     for arch in iter(config.ARCHES.keys()):
         base = Dnf5MadbBase(distro, arch, config.DATA_PATH, refresh=True)
-print(f"Updated {config.APP_NAME} metadata")
+elapsed =  datetime.datetime.now() - start
+print(f"Updated {config.APP_NAME} metadata in {humanize.naturaldelta(elapsed, minimum_unit='seconds')}")
