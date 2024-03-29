6f56474d7becd7cba75980aaadab51961515ae6d
Papoteur <papoteur@mageia.org>
2024-03-29 06:55:06 +0100

    limit verbosity in makecache

--- makecache.py
+++ makecache.py
@@ -6,6 +6,5 @@ import logging
 
 for distro in iter(config.DISTRIBUTION.keys()):
     for arch in iter(config.ARCHES.keys()):
-        print(f"{distro} {arch}" )
         base = Dnf5MadbBase(distro, arch, config.DATA_PATH, refresh=True)
-logging.info("Updated {config.APP_NAME} metadata")
+print(f"Updated {config.APP_NAME} metadata")
