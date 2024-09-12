fe99aa57c96d5de8a54be6178f6faccc2375b4b4
Papoteur <papoteur@mageia.org>
2024-09-12 08:24:52 +0200

    Manage bad screenshots file

--- madb/screenshots.py
+++ madb/screenshots.py
@@ -5,10 +5,16 @@ class Screenshots():
     def __init__(self):
         URL = "http://screenshots.debian.net/json/screenshots"
         content = load_content_or_cache(URL)
-        self.scdb = json.loads(content)["screenshots"]
+        try:
+            self.scdb = json.loads(content)["screenshots"]
+        except JSONDecodeError:
+            self.scdb = None
+            return
         self.keys = [x["name"] for x in self.scdb]
 
     def image_links(self, package):
+        if self.scdb is None:
+            return []
         if package in self.keys:
             links = [ {"small": x["small_image_url"], "large": x["large_image_url"]} for x in self.scdb if x["name"] == package  ]
             return links
