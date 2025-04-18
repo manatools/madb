54616d69e98d922cbeb6802bcefb34386e3977b5
Papoteur <papoteur@mageia.org>
2025-04-18 09:50:25 +0200

    fix: path to makecache in start_madb.sh

--- system/start_madb.sh
+++ system/start_madb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Démarrer plusieurs processus
-/usr/bin/makecache.py -f &
+/var/lib/madb/makecache.py -f &
 /usr/bin/gunicorn --workers=8 --timeout=90 wsgi:madb_app --log-level=INFO --bind=:5003 &
 wait
