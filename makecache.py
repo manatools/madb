e707ad8633ee16c6419cfdb7e42e764b8ec9f23a
Mageia Dev <dev@ml.mageia.org>
2025-07-01 07:27:30 +0200

    Log in specific file for makecache

--- makecache.py
+++ makecache.py
@@ -12,7 +12,13 @@ import argparse
 
 logger = logging.getLogger(__name__)
 log_level = getattr(logging, config.LOG_LEVEL.upper())
-logging.basicConfig(filename=os.path.join(config.LOG_PATH,'madb.log'), encoding='utf-8', level=log_level)
+logging.basicConfig(
+    filename=os.path.join(config.LOG_PATH,'makecache.log'),
+    encoding='utf-8',
+    level=log_level,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    )
 
 
 parser = argparse.ArgumentParser(description = help)
@@ -29,16 +35,15 @@ def makecache():
                         base = Dnf5MadbBase(distro, arch, config.DATA_PATH, refresh=True)
                         message = f"Repository {distro} {arch} up to date"
                         logging.debug(message)
-                        print(message)
                     except Exception as e:
                         logging.warning(f"Updating {config.APP_NAME} metadata for {distro} {arch} failed with:\n{traceback.format_exc()}")
     elapsed =  datetime.datetime.now() - start
     message = f"Updating {config.APP_NAME} metadata took {humanize.naturaldelta(elapsed, minimum_unit='seconds')} at { datetime.datetime.now()}"
     logging.info(message)
-    print(message)
 
 while True:
     makecache()
     if not args.follow:
         break
+    logging.info(f"In pause for {config.MAKE_CACHE_FREQUENCY} min")
     time.sleep(config.MAKE_CACHE_FREQUENCY * 60)
