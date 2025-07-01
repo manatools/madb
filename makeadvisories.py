d573ca61fee7376b71e907d40cc10f8fb8845ac4
Mageia Dev <dev@ml.mageia.org>
2025-07-01 07:26:42 +0200

    Logging in specific file for makeadvisories

--- makeadvisories.py
+++ makeadvisories.py
@@ -12,9 +12,11 @@ import argparse
 logger = logging.getLogger(__name__)
 log_level = getattr(logging, config.LOG_LEVEL.upper())
 logging.basicConfig(
-    filename=os.path.join(config.LOG_PATH, "madb.log"),
+    filename=os.path.join(config.LOG_PATH, "makeadvisories.log"),
     encoding="utf-8",
     level=log_level,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
 
@@ -42,7 +44,7 @@ def makecache():
 
 while True:
     makecache()
-    print("Advisories list update done")
     if not args.follow:
         break
+    logging.info(f"In pause for {config.MAKE_CACHE_FREQUENCY} min")
     time.sleep(config.MAKE_CACHE_FREQUENCY * 60)
