aac4ffe5179cd2f318e2a21de951d89578ac24b4
Mageia Dev <dev@ml.mageia.org>
2025-07-26 16:57:59 +0200

    Launch check_anitya_dnf with log level

--- system/start_madb.sh
+++ system/start_madb.sh
@@ -4,5 +4,5 @@
 # Démarrer plusieurs processus
 /usr/bin/gunicorn --workers=8 --timeout=90 wsgi:madb_app --log-level=INFO --bind=:5003 &
 # process for collecting anitya data: first pass then update process
-/usr/bin/python3 /var/lib/madb/check_anitya_dnf.py -f > /dev/null 2> anitya-error.log; /usr/bin/python3 /var/lib/madb/scheduler.py > /dev/null 2>> anitya-error.log &
+/usr/bin/python3 /var/lib/madb/check_anitya_dnf.py -f -l INFO > /dev/null 2> anitya-error.log; /usr/bin/python3 /var/lib/madb/scheduler.py > /dev/null 2>> anitya-error.log &
 wait
